ee48427e4b9f2bf860a881beac9e739af600a41e
SaurabhN16 <91530878+saurabhnimkande@users.noreply.github.com>
2021-11-12 16:51:34 +0530

    added popup product details  to products page.

--- scripts/singleProductView.js
+++ scripts/singleProductView.js
@@ -1,54 +1,49 @@
-let object={
-    display_img:
-      "https://assets.ajio.com/medias/sys_master/root/20210824/qnyN/6124089d7cdb8cb824031048/performax_navy_blue_running_zip-front_dobby_jacket.jpg",
-    img2: "https://assets.ajio.com/medias/sys_master/root/20210824/Tmuz/6123f3797cdb8cb824028358/-473Wx593H-441128864-darkblue-MODEL3.jpg",
-    img3: "https://assets.ajio.com/medias/sys_master/root/20210824/RhpY/6123fa05f997ddce89a878b3/-473Wx593H-441128864-darkblue-MODEL2.jpg",
-    img4: "",
-    brand: "PERFORMAX",
-    name: "Running Zip-Front Dobby Jacket",
-    price: 899,
-    MRP: 999,
-    discount: 10,
-    offer_price: 849,
-    color: "Dark-Blue",
-    size: "",
-  };
-
-  // localStorage.setItem("bucket",JSON.stringify(object));
+let str1="";
+let str2="";
+let str3="";
+let obj={};
+function displayFromBucket() {
+    let object=JSON.parse(localStorage.getItem("bucket"));
+      
+      // localStorage.setItem("bucket",JSON.stringify(object));
 
 
-let mainDisplayImage = document.getElementById("mainDisplayImage");
-mainDisplayImage.src=object.display_img;
+    let mainDisplayImage = document.getElementById("mainDisplayImage");
+    mainDisplayImage.src=object.display_img;
 
-let extraimage1=document.getElementById("image1extra");
-extraimage1.src=object.display_img;
+    let extraimage1=document.getElementById("image1extra");
+    extraimage1.src=object.display_img;
+    str1=object.display_img;
 
-let extraimage2 = document.getElementById("image2extra");
-extraimage2.src=object.img2;
+    let extraimage2 = document.getElementById("image2extra");
+    extraimage2.src=object.img2;
+    str2=object.img2;
 
-let extraimage3 = document.getElementById("image3extra");
-extraimage3.src=object.img3;
+    let extraimage3 = document.getElementById("image3extra");
+    extraimage3.src=object.img3;
+    str3=object.img3;
 
-let brandName = document.getElementById("brandName");
-brandName.innerText=object.brand;
+    let brandName = document.getElementById("brandName");
+    brandName.innerText=object.brand;
 
-let productTitle=document.getElementById("productTitle");
-productTitle.innerText=object.name;
+    let productTitle=document.getElementById("productTitle");
+    productTitle.innerText=object.name;
 
-let top_divoneline= document.getElementById("top_divoneline");
-if(top_divoneline!=null) {
-    top_divoneline.innerText=object.name;
+    let top_divoneline= document.getElementById("top_divoneline");
+    if(top_divoneline!=null) {
+        top_divoneline.innerText=object.name;
+    }
+
+    let productPrice=document.getElementById("productPrice");
+    productPrice.innerText="Rs. "+object.price;
+
+    let productPriceOff=document.getElementById("productPriceOff");
+    productPriceOff.innerText="Rs. "+object.MRP+" ("+object.discount+"% off)";
+
+    let productColor=document.getElementById("productColor");
+    productColor.innerText=object.color;
+    return object;
 }
-
-let productPrice=document.getElementById("productPrice");
-productPrice.innerText="Rs. "+object.price;
-
-let productPriceOff=document.getElementById("productPriceOff");
-productPriceOff.innerText="Rs. "+object.MRP+" ("+object.discount+"% off)";
-
-let productColor=document.getElementById("productColor");
-productColor.innerText=object.color;
-
 
 
 let itemDetailsDiv=document.getElementById("popupBackgroundBlack");
@@ -134,21 +129,22 @@ function productSizeSelect(value) {
 }
 
 function displayImage1() {
-  mainDisplayImage.src = object.display_img;
+  mainDisplayImage.src = str1;
 }
 function displayImage2() {
-  mainDisplayImage.src = object.img2;
+  mainDisplayImage.src = str2;
 }
 function displayImage3() {
-  mainDisplayImage.src = object.img3;
+  mainDisplayImage.src = str3;
 }
-
+console.log(obj);
 
 function addProductTocart() {
     if (localStorage.getItem("Cart_AJIO") == null) {
         localStorage.setItem("Cart_AJIO", JSON.stringify([]));
       }
     let items=JSON.parse(localStorage.getItem("Cart_AJIO"));
+    let object=displayFromBucket();
     items.push(object);
     localStorage.setItem("Cart_AJIO", JSON.stringify(items));  
     window.location.reload();   
